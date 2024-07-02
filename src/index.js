0cb5905c744061da16533e51e3306f544b081fcf
Abhinav <abhinav.contact22@gmail.com>
2024-07-02 20:40:23 +0530

    Init the bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Working");
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.GuildMessages],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("FUN");
+});
+
+client.login(process.env.CLIENT_TOKEN);
